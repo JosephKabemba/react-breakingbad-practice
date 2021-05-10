4b4e0c40ac83ef1000e06323d3e19a673096bb4c
JosephKabemba <joseph.kabemba@academy.kinshasadigital.com>
2021-05-10 14:41:19 +0100

    feat: retrieve data and set characters state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import "./App.css";
+import { useState, useEffect } from "react";
 import Header from "./components/ui/Header";
+import axios from "axios";
+import "./App.css";
 
 const App = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const result = await axios(
+        "https://www.breakingbadapi.com/api/characters"
+      );
+      // console.log(result.data);
+      setItems(result.data);
+      setIsLoading(false);
+    };
+
+    fetchItems();
+  }, []);
+
   return (
     <div className="container">
       <Header />
